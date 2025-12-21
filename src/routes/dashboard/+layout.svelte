feee7a4f46ef55239d62ffa83735e8ebd8cf353d
Baby Feavel <tanuki20966@gmail.com>
2025-12-21 22:29:38 +0800

    main

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,33 +1,3 @@
-<!-- <script>
-    import Separator from "$lib/components/ui/separator/separator.svelte";
-    import * as Tabs from '$lib/components/ui/tabs/index.js';
-
-    // Define the feed type tabs
-    const feedTypeTabs = [
-      { value: 'article', label: 'Articles', description: 'View public article feeds' },
-      { value: 'product', label: 'Products', description: 'View public product feeds' },
-      { value: 'service', label: 'Services', description: 'View public service feeds' },
-    ];
-
-    // Reactive state for the active tab
-    let activeTab = $state('article');
-
-
-    let { children } = $props();
-
-</script>
-<div class="flex flex-1 flex-col">
-	<div class="flex-1 space-y-6 p-6 md:p-10">
-		<div>
-			<h2 class="text-2xl font-bold tracking-tight">Dashboard</h2>
-			<p class="text-muted-foreground">Some basic info on what is going on</p>
-		</div>
-
-		<Separator />
-
-	</div>
-</div> -->
-
 <script>
 	let { children } = $props();
 </script>
